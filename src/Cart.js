49b6a0fc8a50a7053b67e09ff026d9e8206fc15f
BarkhaAggarwal <barkhag2000@gmail.com>
2021-06-14 18:29:04 +0530

    add cart compo

---
+++ src/Cart.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import CartItem from './CartItem';
+
+class Cart extends React.Component{
+render(){
+    return(
+    <div className='cart'>   
+        <CartItem />
+        <CartItem />
+        <CartItem />
+    </div>
+  
+    );
+}
+
+}
+
+
+
+
+
+
+export default Cart;
